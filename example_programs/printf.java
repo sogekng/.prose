aed4e8f629864c69b3bfd60f1af7b9a18dfbd438
sogekng <gutsantos3@gmail.com>
2024-06-07 23:09:29 -0300

    Tratando erros(não prejudiciais)

--- example_programs/printf.java
+++ example_programs/printf.java
@@ -1,5 +1,13 @@
-public class printf {
-public static void main(String[] args) {
-final String str_world = "World";
-System.out.printf("Hello %s!", str_world);
-}}
+import java.util.Scanner;
+
+public class printf
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        final String str_world = "World";
+        System.out.printf("Hello %s!", str_world);
+        scanner.close();
+    }
+}
